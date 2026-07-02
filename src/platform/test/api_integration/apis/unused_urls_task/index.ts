11341b91f2c85e570f8c58209d34cc40ffc659cc
Tamerlan Gudabayev <tamerlan.gudabayev@elastic.co>
2026-07-02 20:02:29 +0200

    add ftr test to test out smart retry

--- src/platform/test/api_integration/apis/unused_urls_task/index.ts
+++ src/platform/test/api_integration/apis/unused_urls_task/index.ts
@@ -11,6 +11,9 @@ import type { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('unused_urls_task', () => {
+    // TEMPORARY: validates FTR retry intersection logic. Delete before merging this PR.
+    loadTestFile(require.resolve('./retry'));
+
     loadTestFile(require.resolve('./run'));
   });
 }
